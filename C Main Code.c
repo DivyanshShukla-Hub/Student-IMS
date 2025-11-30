7719082832052009316354d9783d2b54559787eb
Divyansh shukla <divyanshshukla765@gmail.com>
2025-11-30 14:33:00 +0000

    helrp to check

--- C Main Code.c
+++ C Main Code.c
@@ -1,2 +1,22 @@
+#include <stdio.h>
+
+#define MAX 50
+
+int main() {
+    int roll;
+    char name[MAX];
+
+    printf("Enter Roll Number: ");
+    scanf("%d", &roll);
+
+    printf("Enter Name: ");
+    scanf(" %[^\n]", name);  // takes full name
+
+    printf("\n--- Student Details ---\n");
+    printf("Roll Number: %d\n", roll);
+    printf("Name: %s\n", name);
+
+    return 0;
+}
 
 
