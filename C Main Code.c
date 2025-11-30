2fdd88a3f7ab3031978b797981c0af5c005f8002
Divyansh shukla <divyanshshukla765@gmail.com>
2025-11-30 14:30:11 +0000

    removal of old code

--- C Main Code.c
+++ C Main Code.c
@@ -1,25 +1,2 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#define MAX_NAME 50
-#define NUM_SUBJECTS 5
-
-typedef struct {
-    int rollNo;
-    char name[MAX_NAME];
-    float marks[NUM_SUBJECTS];
-    float total;
-    float average;
-    char grade;
-} Student;
-
-void inputStudents(Student *students, int n);
-void calculateResults(Student *students, int n);
-char calculateGrade(float avg);
-void displayStudent(const Student *s);
-void displayAll(const Student *students, int n);
-void displayClassSummary(const Student *students, int n);
-int findTopperIndex(const Student *students, int n);
-void clearInputBuffer();
 
